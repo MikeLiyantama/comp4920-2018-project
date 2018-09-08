27dfcfde1db0399a5abd8a5314c5b7eac8a8f2da
Andrew Nguyen <andrew.nguyen.h3@gmail.com>
2018-09-08 12:54:08 +1000

    12: feat: Add POST /api/task endpoint to create tasks

--- server.js
+++ server.js
@@ -35,20 +35,55 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:2701
 
 // API Routers
 
-//Basic authentication
-router.post('/auth', function(req, res) {
-    if(req.body.email && req.body.password){   //Field Check
-        db.collection('users').findOne({email: req.body.email}, function (err, result) {
+// error handler
+function returnError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({ "error": message });
+}
+
+/**
+  USERS
+ */
+
+var USERS_COLLECTION = 'USERS';
+
+router.post('/api/auth', function(req, res) {
+    if (req.body.email && req.body.password) { // Field Check
+        db.collection(USERS_COLLECTION).findOne({ email: req.body.email }, function (err, result) {
             if (result) {
-                //console.log("Email Found");
-                if(req.body.password == result.password){
-                    res.status(200).json({success: true});
-                } else res.json({success: false});
-            }
-            else {
-                //console.log("Email Not Found");
+                if (req.body.password == result.password) {
+                  res.status(200).json({ success: true });
+                } else {
+                  res.json({ success: false });
+                }
+            } else {
                 res.status(200).json({success: false});
             }
         });
-    } else res.status(400).json({success: false}); //Bad request, incomplete field
+    } else {
+      returnError(res, 'Invalid user input', 'Must provide email and password', 400);
+    }
+});
+
+/**
+  TASKS
+ */
+
+var TASKS_COLLECTION = 'TASKS';
+
+app.post('/api/task', function (req, res) {
+  var newTask = req.body;
+  newTask.createdAt = new Date();
+
+  if (!req.body.title) {
+    returnError(res, 'Invalid user input', 'Must provide a title', 400);
+  } else {
+    db.collection(TASKS_COLLECTION).insertOne(newTask, function (err, doc) {
+      if (err) {
+        returnError(res, err.message, "Failed to create new task");
+      } else {
+        res.status(201).json(doc.ops[0]);
+      }
+    });
+  }
 });
