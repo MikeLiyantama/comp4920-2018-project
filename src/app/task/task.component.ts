55c1b5acaf2220d23c182ee4c0b36aef908555e2
onuralpbicer <onuralpbicer@gmail.com>
2018-09-13 15:25:18 +1000

    Added a 'TaskComponent' which is a placeholder for the actual app screen after login. It is just a dummy component for now, to test login and redirection

---
+++ src/app/task/task.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+//This is just a placeholder component to make sure login and redirection works
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
