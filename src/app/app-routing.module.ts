6a04484c05f019670b0b0b661f91e8eb4592a376
Andrew Nguyen <andrew.nguyen.h3@gmail.com>
2018-09-17 14:26:16 +1000

    fix after rebasing onto master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent }       from './login/login.component';
-import { TaskComponent }       from './task/task.component';
+import { TaskComponent } from './tasklistC/taskdispC/taskC/task.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
