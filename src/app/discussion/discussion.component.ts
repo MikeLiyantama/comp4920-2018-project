41b9ee71195f84a70d355b92ee53759ffa6d3b11
Kevin Au <z5098891@ad.unsw.edu.au>
2018-10-15 12:30:46 +1100

    pushing changes to discussion ahead of meeting

--- src/app/discussion/discussion.component.ts
+++ src/app/discussion/discussion.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
+import { FormBuilder, FormControl, Validators, FormGroup } from '@angular/forms';
+import { Message } from './message/message.model';
+import { User } from '../user/user.model';
 
 @Component({
     selector: 'app-discussion',
@@ -6,7 +9,39 @@ import { Component } from '@angular/core';
     styleUrls: ['./discussion.component.css']
 })
 
-export class DiscussionComponent {
+export class DiscussionComponent implements OnInit {
+    @Input () currentUser: User;
+    @Input () messagesToRender;
+    @Output () commentEmitter = new EventEmitter <Message>();
+    sendHighlight="primary"
+    comment: string;
+    commentFormGroup;
 
+    constructor (private _formBuilder: FormBuilder){}
+
+    ngOnInit () {
+        this.commentFormGroup = this._formBuilder.group({
+            CommentCtrl: ['', Validators.required]
+        });
+    }
+
+    resetComment () {
+        this.commentFormGroup.controls["CommentCtrl"].reset();
+    }
+
+    postComment () {
+        var comment = this.commentFormGroup.controls["CommentCtrl"].value;
+        if (comment) {
+            // Disregard empty comments otherwise
+            var date = new Date();
+            var msg = new Message (comment, this.currentUser, date);
+            this.commentEmitter.emit (msg);
+            
+            // TODO!!!!! SET THE CURRENT USER CORRECTLY FROM TEAMDETAIL ts
+
+            console.log (this.currentUser);
+        }
+        
+    }
 
 }
