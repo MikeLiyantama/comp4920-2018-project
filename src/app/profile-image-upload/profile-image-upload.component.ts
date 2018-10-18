d12b251f54bf579a56ac1c0700cd0164d9926f8a
Michael Liyantama <michael.liyantama@live.com>
2018-10-19 00:06:28 +1100

    Re-merge #89 with bug fix

--- src/app/profile-image-upload/profile-image-upload.component.ts
+++ src/app/profile-image-upload/profile-image-upload.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-profile-image-upload',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-image-upload.component.css']
 })
 export class ProfileImageUploadComponent implements OnInit {
+  imgFile: File;     
+  @Input () buttonLine: string;
+  @Output () imgEmitter: EventEmitter<string> = new EventEmitter <string>();
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  fileChanged (event: any) {
+    var reader = new FileReader();
+    this.imgFile = event.target.files[0];
+    reader.readAsDataURL(this.imgFile)
+    reader.onload = (event) => {
+    var img = <FileReader>event.target;
+        this.imgEmitter.emit (img.result); 
+    }
+}
 
 }
